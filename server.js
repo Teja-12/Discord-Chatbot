69d4607c5adb47fd4d43938e7c93085da638bdd9
Glitch (hello-express) <none>
2020-07-27 05:22:09 +0000

    🙈🦀 Checkpoint ./server.js:19003137/234 ./package.json:19003137/2634

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const discord = require("discord.js")
 const client = new discord.client()
-
+const alexa = require('alexa-bot-api')
+var chatbot = new alexa('aw2plm') //free access key for Mewtwo Machine channel (provided by CTK WARRIOR)
 client.on('ready', () => {
   console.log('Ready for chatting!| Bot by 0_0')
 });
 
-client.on('message')
+client.on('message', async message => {
+  let content = message.content
+  
+})
           
