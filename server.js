52fab2b7c769c24247515528ad1ea5e2643d8366
Glitch (hello-express) <none>
2021-04-13 07:14:44 +0000

    🐚🎊 Checkpoint ./server.js:19003137/1580

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 const alexa = require("alexa-bot-api");
-var chatbot = new alexa(); // Setting up the client 
+var chatbot = new alexa(); // Setting up the client
 client.on("ready", () => {
   console.log("Ready for chatting!| Bot by 0_0");
 });
@@ -9,7 +9,7 @@ client.on("message", async message => {
   // when client detects a message
   if (message.author.bot) return; // if the author of the message is a bot ignore the case
   let content = message.content; // let content ( a variable used to fetch response ) be equal to the message's content
-  chatbot.getReply(content, "english").then(response =>
+  chatbot.getReply(content, "en").then(response =>
     message.channel.send(`${message.author}`, {
       embed: {
         description: `${response}`
